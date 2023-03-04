2899f0b228575cc5e84a105653b01b8a67f4bbf2
ana.mjeda <anamjeda@gmail.com>
2023-03-04 14:08:54 +0100

    Initial commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    protected WebDriverWait driverWait;
+
+    private final String baseURL = "https://the-internet.herokuapp.com";
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get(baseURL);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
